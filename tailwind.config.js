40e764b51ba3c971c1dc8446ce5e794f1969fc68
Rastislav Kudla <rasto@snowflakesolutions.eu>
2025-02-24 20:30:12 +0100

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{html,ts}" // Ensures Tailwind scans all component templates
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
